878d12d10343ce54fa57ae7ec378c091383d8e86
Nihan-ali <nihansust11@gmail.com>
2024-01-05 02:55:00 +0600

    Some Frontend

---
+++ SmartClinic/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartClinic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string TodayDate { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            // Set TodayDate to the current date or the desired date format
+            TodayDate = DateTime.Now.ToString("dd-MM-yyyy");
+        }
+        private void OnSearchTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchPatientTextBox.Text == "Search Patient")
+            {
+                searchPatientTextBox.Text = "";
+                searchPatientTextBox.Foreground = Brushes.Black; // Change the color to the regular text color
+            }
+        }
+
+        private void OnSearchTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchPatientTextBox.Text))
+            {
+                searchPatientTextBox.Text = "Search Patient";
+                searchPatientTextBox.Foreground = Brushes.Gray; // Change the color to the placeholder color
+            }
+        }
+
+    }
+}
