b87a296e4966da371e600fc29d0e20d0560e44b0
Nihan-ali <nihansust11@gmail.com>
2024-01-26 19:03:10 +0600

    random changes

--- SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
+++ SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b53af681ef02dcec5c78dd09e14b2ae28b650dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42a1e08c164058160872577a3509a49a970f2a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
