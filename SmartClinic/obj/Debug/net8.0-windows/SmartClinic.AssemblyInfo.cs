b625fa20bc4b1ec09c6e12845e4c30a502c74b32
Nihan-ali <nihansust11@gmail.com>
2024-02-01 03:06:21 +0600

    Patinet list added

--- SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
+++ SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b9f3dab8e0bbb971c2e6ad0e9e46d76c3f23bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49e2dd4a8de3583a1cf967c5c634a58ca26ba33")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
