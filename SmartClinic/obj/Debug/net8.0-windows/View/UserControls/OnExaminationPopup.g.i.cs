2e07a7581fd2ca2f02eba6309427c0a82b91ea97
Shishir1035 <shishirshohanuzzaman@gmail.com>
2024-01-31 06:01:59 +0600

    db simplified

--- SmartClinic/obj/Debug/net8.0-windows/View/UserControls/OnExaminationPopup.g.i.cs
+++ SmartClinic/obj/Debug/net8.0-windows/View/UserControls/OnExaminationPopup.g.i.cs
@@ -85,7 +85,7 @@ namespace SmartClinic.View.UserControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SmartClinic;V1.0.0.0;component/view/usercontrols/onexaminationpopup.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SmartClinic;component/view/usercontrols/onexaminationpopup.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\UserControls\OnExaminationPopup.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
