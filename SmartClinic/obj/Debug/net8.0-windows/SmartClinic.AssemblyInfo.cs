bb93057b17c30b30b4feea061922f10ef04b69d9
Nihan-ali <nihansust11@gmail.com>
2024-03-03 17:35:26 +0600

    again new branch

--- SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
+++ SmartClinic/obj/Debug/net8.0-windows/SmartClinic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586f50dfd7314590785ac005f1023e6724ccfbd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269fc3c5b2982c88baa439f8858e08ec5e67d283")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
