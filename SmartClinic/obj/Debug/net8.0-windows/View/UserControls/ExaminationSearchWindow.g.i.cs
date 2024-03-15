b47aa0c090be73371694dd70603dbec29f280a96
Shishir1035 <shishirshohanuzzaman@gmail.com>
2024-03-16 05:33:32 +0600

    ar parina

--- SmartClinic/obj/Debug/net8.0-windows/View/UserControls/ExaminationSearchWindow.g.i.cs
+++ SmartClinic/obj/Debug/net8.0-windows/View/UserControls/ExaminationSearchWindow.g.i.cs
@@ -79,13 +79,13 @@ namespace SmartClinic.View.UserControls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SmartClinic;component/view/usercontrols/examinationsearchwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SmartClinic;V1.0.0.0;component/view/usercontrols/examinationsearchwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\UserControls\ExaminationSearchWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -95,7 +95,7 @@ namespace SmartClinic.View.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
@@ -144,7 +144,7 @@ namespace SmartClinic.View.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
